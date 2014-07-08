c9afcd26f6a38c812992505d81ff415aae2c10e5
Ashe Magalhaes <amagalhaes@fb.com>
2014-07-08 10:30:00 -0700

    Added pie chart

---
+++ Pie Chart/Pie ChartTests/Pie_ChartTests.m
@@ -0,0 +1,34 @@
+//
+//  Pie_ChartTests.m
+//  Pie ChartTests
+//
+//  Created by Ashe Magalhaes on 7/8/14.
+//  Copyright (c) 2014 Facebook. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Pie_ChartTests : XCTestCase
+
+@end
+
+@implementation Pie_ChartTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
